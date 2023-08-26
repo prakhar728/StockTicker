04614035a2e02e989f53a051f2db2c532d60bc5d
Prakhar <prakharojha12@gmail.com>
2023-08-26 11:11:50 +0530

    Modified to remove unecessary comments'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,22 +7,16 @@ public class Main {
     public static void main(String[] args) {
 	//let's try and give this code  a whirl
         try{
-            String urlString = "http://w    ww.nseindia.com/content/historical/EQUITIES/2015/JUL/cm17JUL2015bhav.csv.zip";
+            String urlString = "http://www.nseindia.com/content/historical/EQUITIES/2015/JUL/cm17JUL2015bhav.csv.zip";
             String zipFilePath = "/Users/swethakolalapudi/JavaDrills/boo.zip";
             String destinationDirectory="/Users/swethakolalapudi/JavaDrills/";
 
-            // note this way of specifying paths in java - even if this code were being written on a windows
-            // machine it would still be fine to use the unix file separator('/'). On windows machines
-            // the DOS/Windows file separator ('\') will work too, but will need to be 'escaped'
-            // with an extra backslash Ex: "C:\\Users\\sk\\Drills\Test.xls"
+
 
             List<String> unzippedFileList = UnzipUtility.downloadAndUnzip(urlString, zipFilePath, destinationDirectory);
-            // calling the method we just wrote here. Remember this function's signature includes an exception?
-            // this line would throw an error if we did not surround the code with a try/catch block
 
-            // let's try and give this code a spin
+
             if (unzippedFileList!=null){
-                // only do this if we actually unzipped anything
                 String csvFile = unzippedFileList.get(0);
                 OneDayMarketAction odma = new OneDayMarketAction(csvFile);
                 // use the constructor of the class we just defined
